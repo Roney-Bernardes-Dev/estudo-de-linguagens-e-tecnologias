9c7e299bdc7cd78442164df459d8f2a8e4e7b1c4
Roney Bernardes <83255781+Roney-Bernardes-Dev@users.noreply.github.com>
2023-02-12 11:47:44 -0300

    Update main.js

--- Alert/ParseInt/Somando/main.js
+++ Alert/ParseInt/Somando/main.js
@@ -1,5 +1,5 @@
 const primeiroNumero = prompt('Digite o primeiro número: ');
-primeiroNumero = parseInt(primeiroNumero);
+const primeiro = parseInt(primeiroNumero);
 const segundoNumero = prompt('Digite o segundo número: ');
-segundoNumero = parseInt(segundoNumero);
-alert(primeiroNumero + segundoNumero); 
+const segundo = parseInt(segundoNumero);
+alert(primeiro + segundo); 
