7d51e1e653a2c8bbbfddd02e57fc4ad99379537c
Roney Bernardes <83255781+Roney-Bernardes-Dev@users.noreply.github.com>
2023-02-12 10:18:42 -0300

    testando alert

---
+++ Alert/main.js
@@ -0,0 +1,8 @@
+const name = prompt('Qual o seu nome: ?')
+const nameBlock = ['joao', 'maria', 'jose']
+
+    if (nameBlock.includes(name)) {
+        alert('Esse nome já existe tente outro')
+    }else {
+        alert('Bem Vindo(a). ' + name)
+    }
