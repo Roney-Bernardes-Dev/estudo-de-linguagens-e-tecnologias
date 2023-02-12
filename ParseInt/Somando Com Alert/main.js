1362cc769a215e57766876b7f31d32dc85b449ec
Roney Bernardes <83255781+Roney-Bernardes-Dev@users.noreply.github.com>
2023-02-12 11:28:58 -0300

    parseInt

---
+++ ParseInt/Somando Com Alert/main.js
@@ -0,0 +1,5 @@
+const primeiroNumero = prompt('Digite o primeiro número: ');
+const primeiro = parseInt(primeiroNumero);
+const segundoNumero = prompt('Digite o segundo número: ');
+const segundo = parseInt(segundoNumero);
+alert(primeiro + segundo); 
