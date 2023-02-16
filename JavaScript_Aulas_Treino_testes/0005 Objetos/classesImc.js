d81940800e48d5eb1f8bc545f39125c13c46e8e2
Roney-bernarde-Dev <83255781+Roney-Bernardes-Dev@users.noreply.github.com>
2023-02-16 09:04:54 -0300

    Update classesImc.js

--- JavaScript_Aulas_Treino_testes/0005 Objetos/classesImc.js
+++ JavaScript_Aulas_Treino_testes/0005 Objetos/classesImc.js
@@ -12,7 +12,23 @@ class Pessoa {
     calcularImc() {
         return this.peso / (this.altura * this.altura); 
     }
+
+    classificarImc() {
+        const imc = this.calcularImc();
+        if (imc < 15.5) {
+            return 'Abaixo do peso'; 
+        } else if (imc >= 18.5 && imc < 25) {
+             return 'Peso Normal';
+        } else if (imc >= 25  && imc < 30) {
+             return 'Acima do Peso';
+        } else if (imc >= 30  && imc < 40) {
+             return 'Obeso';
+        } else {
+             return 'Obsidade Grave';
+        }
+    }
 }
 
-const roney = new Pessoa ('Roney', 69, 1.69)
+const roney = new Pessoa ('Roney', 69, 1.69);
 console.log(roney.calcularImc().toFixed(2));
+console.log(roney.classificarImc());
