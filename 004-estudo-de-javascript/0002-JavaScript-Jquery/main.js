afeb919a894ec3bfe3ada25a84126879c2bedc60
Roney Bernardes <83255781+Roney-Bernardes-Dev@users.noreply.github.com>
2023-02-25 16:57:50 -0300

    Create main.js

---
+++ 004-estudo-de-javascript/0002-JavaScript-Jquery/main.js
@@ -0,0 +1,12 @@
+var a = 25;
+var b = 75;
+
+console.log(a + b);
+
+function areaCirculo(raio) {
+    var pi = 3.14, 
+        area = pi * raio * raio;
+    return area
+    
+}
+console.log(areaCirculo(15));
