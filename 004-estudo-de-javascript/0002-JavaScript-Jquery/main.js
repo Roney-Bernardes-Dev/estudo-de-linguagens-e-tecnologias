d1944f84917da0938d2c8029e825e7ffb40e40fe
Roney Bernardes <83255781+Roney-Bernardes-Dev@users.noreply.github.com>
2023-02-25 17:56:46 -0300

    Jquery

--- 004-estudo-de-javascript/0002-JavaScript-Jquery/main.js
+++ 004-estudo-de-javascript/0002-JavaScript-Jquery/main.js
@@ -1,13 +1,1 @@
-var a = 25;
-var b = 75;
 
-console.log(a + b);
-
-function areaCirculo(raio) {
-    var pi = 3.14, 
-        area = pi * raio * raio;
-    return area
-    
-}
-
-console.log(areaCirculo(15));
