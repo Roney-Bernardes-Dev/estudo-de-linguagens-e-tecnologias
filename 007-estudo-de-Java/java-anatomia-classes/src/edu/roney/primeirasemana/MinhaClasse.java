a3853d30e35ebb34a7c2c85da67266524f517638
Roney Bernardes <83255781+Roney-Bernardes-Dev@users.noreply.github.com>
2023-02-25 09:55:22 -0300

    Update MinhaClasse.java

--- 007-estudo-de-Java/java-anatomia-classes/src/edu/roney/primeirasemana/MinhaClasse.java
+++ 007-estudo-de-Java/java-anatomia-classes/src/edu/roney/primeirasemana/MinhaClasse.java
@@ -2,7 +2,7 @@ package edu.roney.primeirasemana;
 
 public class MinhaClasse {
 public static void main (String [] args) {
-    System.out.print ("Olá, Mundo");
+    System.out.print ("Olá, Mundo lindo");
 
     }
 }
