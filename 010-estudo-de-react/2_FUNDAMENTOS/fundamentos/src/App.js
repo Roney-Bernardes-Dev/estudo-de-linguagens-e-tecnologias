a8b38fc0da681dea7281776c51ab55e5d1fd0de2
Roney Bernardes <83255781+Roney-Bernardes-Dev@users.noreply.github.com>
2023-02-25 21:10:27 -0300

    Fundamentos em React

---
+++ 010-estudo-de-react/2_FUNDAMENTOS/fundamentos/src/App.js
@@ -0,0 +1,16 @@
+// Components
+import FirstComponents from './components/FirstCompponents';
+
+// Styles /CSS
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Fundamentos React</h1>
+      <FirstComponents />
+    </div>
+  );
+}
+
+export default App;
---
+++ 010-estudo-de-react/2_FUNDAMENTOS/fundamentos/src/components/FirstCompponents.js
@@ -0,0 +1,9 @@
+const FirstComponents = () => {
+    return (
+        <div>
+            <h1>Meu Primeiro Componente</h1>
+        </div>
+    );
+};
+
+export default FirstComponents;
