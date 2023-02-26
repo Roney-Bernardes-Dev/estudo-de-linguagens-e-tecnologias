c129999d9c19e313962beb2178c9294749d7391e
Roney Bernardes <83255781+Roney-Bernardes-Dev@users.noreply.github.com>
2023-02-26 20:28:40 -0300

    ligar, desligar

--- 007-estudo-de-Java/smartTv/src/SmartTv.java
+++ 007-estudo-de-Java/smartTv/src/SmartTv.java
@@ -3,5 +3,13 @@ public class SmartTv {
     boolean lingada = false;
     int canal = 1;
     int volume = 25;
-    
+
+    public void ligar() {
+        lingada = true;
+    }
+
+    public void desligar() {
+        lingada = false;
+    }
+
 }
