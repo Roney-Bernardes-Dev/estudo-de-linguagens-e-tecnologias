5a1c5b553c45053873ca8249cf998f6f5ae74858
Roney Bernardes <83255781+Roney-Bernardes-Dev@users.noreply.github.com>
2023-02-28 10:49:14 -0300

    Update Exercises_04_ParEImpar.java

--- 007-estudo-de-Java/java-anatomia-classes/src/edu/roney/fixationexercises/Exercises_04_ParEImpar.java
+++ 007-estudo-de-Java/java-anatomia-classes/src/edu/roney/fixationexercises/Exercises_04_ParEImpar.java
@@ -9,5 +9,32 @@ import java.util.Scanner;
  */
 
 public class Exercises_04_ParEImpar {
+
+    public static void main(String[] args) {
+        try (Scanner scan = new Scanner(System.in)) {
+            
+            int quantidadeNumero;
+            int numero;
+            int quantidadePares = 0, quantidadeImpares = 0;
+
+            System.out.println("Quantidade de números: ");
+            quantidadeNumero = scan.nextInt();
+
+            int count = 0;
+            do {
+                System.out.println("Número: ");
+                numero = scan.nextInt();
+
+                if (numero % 2 == 0) quantidadePares++;
+                else quantidadeImpares++;
+
+                count++;
+            } while(count < quantidadeNumero);
+
+            System.out.println("Quantidade de números pares: " + quantidadePares);
+            System.out.println("Quantidade de números impares: " + quantidadeImpares);
+        }
+
+    }
     
 }
