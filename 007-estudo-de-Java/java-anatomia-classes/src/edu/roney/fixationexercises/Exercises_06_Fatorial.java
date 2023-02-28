12bce128eafe43b58248543a45c20785f7d4259c
Roney Bernardes <83255781+Roney-Bernardes-Dev@users.noreply.github.com>
2023-02-28 11:27:17 -0300

    Update Exercises_06_Fatorial.java

--- 007-estudo-de-Java/java-anatomia-classes/src/edu/roney/fixationexercises/Exercises_06_Fatorial.java
+++ 007-estudo-de-Java/java-anatomia-classes/src/edu/roney/fixationexercises/Exercises_06_Fatorial.java
@@ -1,5 +1,13 @@
 package edu.roney.fixationexercises;
 
+import java.util.Scanner;
+
 public class Exercises_06_Fatorial {
     
+    public static void main(String[] args) {
+        try(Scanner scan = new Scanner(System.in)) {
+
+            
+        }
+    }
 }
