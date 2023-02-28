f9a506160af70970a21b9e41878fecd770c3eb94
Roney Bernardes <83255781+Roney-Bernardes-Dev@users.noreply.github.com>
2023-02-28 08:54:27 -0300

    Create Exercises_003_MaiorEMedia.java

---
+++ 007-estudo-de-Java/java-anatomia-classes/src/edu/roney/fixationexercises/Exercises_003_MaiorEMedia.java
@@ -0,0 +1,12 @@
+package edu.roney.fixationexercises;
+
+/*
+ * Faça um programa que leia 5 números
+ * Informe o maior número
+ * e a média desses npumeros
+ */
+
+public class Exercises_003_MaiorEMedia {
+
+    
+}
