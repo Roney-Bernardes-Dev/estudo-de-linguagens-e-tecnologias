40abcd15181d7177b854f4bd530ca038117a8449
Roney Bernardes <83255781+Roney-Bernardes-Dev@users.noreply.github.com>
2023-02-28 11:45:24 -0300

    Update Exercises_06_Fatorial.java

--- 007-estudo-de-Java/java-anatomia-classes/src/edu/roney/fixationexercises/Exercises_06_Fatorial.java
+++ 007-estudo-de-Java/java-anatomia-classes/src/edu/roney/fixationexercises/Exercises_06_Fatorial.java
@@ -6,8 +6,19 @@ public class Exercises_06_Fatorial {
     
     public static void main(String[] args) {
         try(Scanner scan = new Scanner(System.in)) {
-
             
+            System.out.println("Digite um número para ver o fatorial: ");
+            int fatorial = scan.nextInt();
+
+            int multiplicacao = 1;
+
+            System.out.println(fatorial + "! = ");
+            for (int i = fatorial; i >= 1; i--) {
+                multiplicacao = multiplicacao * i;
+
+            }
+            
+            System.out.println(multiplicacao);
         }
     }
 }
