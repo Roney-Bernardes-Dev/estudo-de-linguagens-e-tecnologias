f4bd57e6a1fd5a84fbffb3e71bce059d20cba074
Roney Bernardes <83255781+Roney-Bernardes-Dev@users.noreply.github.com>
2023-02-28 10:22:38 -0300

    Create Exercises_04_ParEImpar.java

---
+++ 007-estudo-de-Java/java-anatomia-classes/src/edu/roney/fixationexercises/Exercises_04_ParEImpar.java
@@ -0,0 +1,13 @@
+package edu.roney.fixationexercises;
+
+import java.util.Scanner;
+
+/*
+ * Faça um programa que peça N números inteiro
+ * calcule e mostre a quantidade de números pares
+ * e a quantidade de números impares.
+ */
+
+public class Exercises_04_ParEImpar {
+    
+}
