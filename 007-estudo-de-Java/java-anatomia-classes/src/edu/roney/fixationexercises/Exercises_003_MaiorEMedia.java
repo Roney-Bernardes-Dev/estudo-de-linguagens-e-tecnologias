7a352c68040b475c6fa9e8c5b147acddb159ed90
Roney Bernardes <83255781+Roney-Bernardes-Dev@users.noreply.github.com>
2023-02-28 10:11:31 -0300

    Update Exercises_003_MaiorEMedia.java

--- 007-estudo-de-Java/java-anatomia-classes/src/edu/roney/fixationexercises/Exercises_003_MaiorEMedia.java
+++ 007-estudo-de-Java/java-anatomia-classes/src/edu/roney/fixationexercises/Exercises_003_MaiorEMedia.java
@@ -11,7 +11,26 @@ import java.util.Scanner;
 public class Exercises_003_MaiorEMedia {
     public static void main(String[] args) {
         try (Scanner scan = new Scanner(System.in)) {
-            
+
+            int numero;
+            int maior = 0;
+            int soma = 0;
+
+            int count = 0;
+            do {
+                System.out.println("Número: ");
+                numero = scan.nextInt();
+
+                soma = soma + numero;
+
+                if (numero > maior) maior = numero;
+                count = count +1;
+
+            } while(count < 5);
+
+            System.out.println("Maior: " + maior);
+            System.out.println("Media: " + (soma/5));
+
             
         }
         
