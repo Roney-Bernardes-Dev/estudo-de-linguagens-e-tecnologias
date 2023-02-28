be0d0f54f74563b7e86c459255a0d98681cdd3a7
Roney Bernardes <83255781+Roney-Bernardes-Dev@users.noreply.github.com>
2023-02-28 08:46:37 -0300

    Exercises_002_Nota

--- 007-estudo-de-Java/java-anatomia-classes/src/edu/roney/fixationexercises/Exercises_001_NomeEIdade.java
+++ 007-estudo-de-Java/java-anatomia-classes/src/edu/roney/fixationexercises/Exercises_001_NomeEIdade.java
@@ -2,7 +2,7 @@ package edu.roney.fixationexercises;
 
 import java.util.Scanner;
 
-public class Exercio_001_NomeEIdade {
+public class Exercises_001_NomeEIdade {
     public static void main(String[] args) {
         
         try (Scanner scan = new Scanner(System.in)) {
