6e882b8eabdddcc8fbc3f5070712c9e8affebd45
Roney Bernardes <83255781+Roney-Bernardes-Dev@users.noreply.github.com>
2023-02-28 08:22:58 -0300

    Update Exercio_001_NomeEIdade.java

--- 007-estudo-de-Java/java-anatomia-classes/src/edu/roney/primeirasemana/Exercio_001_NomeEIdade.java
+++ 007-estudo-de-Java/java-anatomia-classes/src/edu/roney/primeirasemana/Exercio_001_NomeEIdade.java
@@ -4,18 +4,25 @@ import java.util.Scanner;
 
 public class Exercio_001_NomeEIdade {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-
-        String nome;
-        int idade;
-
-        while() {
+        
+        try (Scanner scan = new Scanner(System.in)) {
+            String nome;
+            int idade;
             
-        }
-        System.out.println("Nome: ");
-        nome = scan.next();
 
-        System.out.println("Idade: ");
-        idade = scan.nextInt();
+            while(true) {
+
+                System.out.println("Nome: ");
+                nome = scan.next();
+                System.out.println("Seu nome nome é: " + nome);
+                if (nome.equals("0")) break;
+
+                System.out.println(" ");
+
+                System.out.println("Idade: ");
+                idade = scan.nextInt();
+                System.out.println("Seu nome nome é: " + idade);
+            }
+        }
     }
 }
