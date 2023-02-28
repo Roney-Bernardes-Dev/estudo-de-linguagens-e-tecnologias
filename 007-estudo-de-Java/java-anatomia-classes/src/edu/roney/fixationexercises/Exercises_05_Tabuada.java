458cb791afe995fe021b1844b2101ce3e7809ccc
Roney Bernardes <83255781+Roney-Bernardes-Dev@users.noreply.github.com>
2023-02-28 11:01:06 -0300

    Update Exercises_05_Tabuada.java

--- 007-estudo-de-Java/java-anatomia-classes/src/edu/roney/fixationexercises/Exercises_05_Tabuada.java
+++ 007-estudo-de-Java/java-anatomia-classes/src/edu/roney/fixationexercises/Exercises_05_Tabuada.java
@@ -1,5 +1,24 @@
 package edu.roney.fixationexercises;
 
+/*
+ * Desenvolva um gerador de tabuada
+ * capaz de gerar tabuadas de qualquer número entre 1 a 10
+ * o usuario deve informar qual o número da tabuada ele deseja ver
+ * a saida deve ser coforme o exemplo abaixo
+ 
+ Tabuada de 5:
+
+ 5 x 1 = 5
+ 5 x 1 = 10
+...
+ 5 x 10 = 50
+ */
+
+
 public class Exercises_05_Tabuada {
+
+    public static void main(String[] args) {
+        
+    }
     
 }
