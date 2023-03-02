9879fc4779ade29639beb8e64fb2bb7415821baf
Roney Bernardes <83255781+Roney-Bernardes-Dev@users.noreply.github.com>
2023-03-02 13:10:58 -0300

    update name

--- 007-estudo-de-Java/java-anatomia-classes/src/edu/roney/fixationexercises/Exercises_08_Foreach_consoantes.java
+++ 007-estudo-de-Java/java-anatomia-classes/src/edu/roney/fixationexercises/Exercises_08_Foreach_consoantes.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
  * imprima as consoantes
  */
 
-public class Exercises_08_Vetor_consoantes {
+public class Exercises_08_Foreach_consoantes {
     public static void main(String[] args) {
        try (Scanner scan = new Scanner(System.in)) {
 
