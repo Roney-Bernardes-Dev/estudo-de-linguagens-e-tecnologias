b90e86b97c3466678d5ad66c1b6359907310def0
Roney Bernardes <83255781+Roney-Bernardes-Dev@users.noreply.github.com>
2023-03-02 13:42:24 -0300

    Create Exercises_10_Array_Multidimensional.java

---
+++ 007-estudo-de-Java/java-anatomia-classes/src/edu/roney/fixationexercises/Exercises_10_Array_Multidimensional.java
@@ -0,0 +1,18 @@
+package edu.roney.fixationexercises;
+
+import java.util.Random;
+
+/*
+ * Gere e imprima um matriz M 4x4 com valores aleatrorios entre 0-9
+ */
+
+public class Exercises_10_Array_Multidimensional {
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        int[][] M = new int[4][4];
+
+        
+    }
+    
+}
