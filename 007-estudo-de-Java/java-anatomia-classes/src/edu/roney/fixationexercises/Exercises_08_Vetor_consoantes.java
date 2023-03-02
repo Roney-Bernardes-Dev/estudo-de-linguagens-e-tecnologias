3e2e992211615120a509ae5f60dd82b5ac4d451b
Roney Bernardes <83255781+Roney-Bernardes-Dev@users.noreply.github.com>
2023-03-02 12:21:49 -0300

    Create Exercises_08_Vetor_consoantes.java

---
+++ 007-estudo-de-Java/java-anatomia-classes/src/edu/roney/fixationexercises/Exercises_08_Vetor_consoantes.java
@@ -0,0 +1,12 @@
+package edu.roney.fixationexercises;
+
+/*
+ * faça um programa que que leia um vetor de 6 caracteres
+ * digite quantas consoantes foram lidas
+ * imprima as consoantes
+ */
+
+public class Exercises_08_Vetor_consoantes {
+    
+    
+}
