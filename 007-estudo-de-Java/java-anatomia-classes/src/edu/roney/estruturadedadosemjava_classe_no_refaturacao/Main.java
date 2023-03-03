17bb4a0b376eb5dab105811daa673e166fb79d03
Roney Bernardes <83255781+Roney-Bernardes-Dev@users.noreply.github.com>
2023-03-02 22:05:11 -0300

    rebilling

---
+++ 007-estudo-de-Java/java-anatomia-classes/src/edu/roney/estruturadedadosemjava_classe_no_refaturacao/Main.java
@@ -0,0 +1,16 @@
+package edu.roney.estruturadedadosemjava_classe_no_refaturacao;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Fila minhaFila = new Fila();
+
+        minhaFila.enqueue(new No("primeiro"));
+        minhaFila.enqueue(new No("segundo"));
+        minhaFila.enqueue(new No("terceiro"));
+        minhaFila.enqueue(new No("quarto"));
+
+        System.out.print(minhaFila);
+        System.out.println(minhaFila.dequieue());
+    }
+}
