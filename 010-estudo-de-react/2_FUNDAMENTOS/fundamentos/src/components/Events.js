bfdd7ff2edf651b1c5c222e956f518b2586634ec
Roney Bernardes <83255781+Roney-Bernardes-Dev@users.noreply.github.com>
2023-03-13 11:35:07 -0300

    creater Events

---
+++ 010-estudo-de-react/2_FUNDAMENTOS/fundamentos/src/components/Events.js
@@ -0,0 +1,15 @@
+const Events = () => {
+    const handMyEvents = (e) => {
+        console.log("Ativado");
+    };
+
+    return (
+        <div>
+            <div>
+                <button>Clique aqui</button>
+            </div>
+        </div>
+    );
+};
+
+export default Events
