7145aab480b4dbe9b2d7d663126618dcb47cfa3d
Roney Bernardes <83255781+Roney-Bernardes-Dev@users.noreply.github.com>
2023-03-13 10:58:46 -0300

    Update TemplateExpressions.js

--- 010-estudo-de-react/2_FUNDAMENTOS/fundamentos/src/components/TemplateExpressions.js
+++ 010-estudo-de-react/2_FUNDAMENTOS/fundamentos/src/components/TemplateExpressions.js
@@ -1,9 +1,14 @@
 const TemplateExpressions = () => {
-    const name = "Matheus";
+    const name = "Roney";
+    const data = {
+        age: 30,
+        job: "programmer",
+    }
 
     return ( 
     <div>
         <h1>Olá {name}, tudo bem?</h1>
+        <p>Sua profissão é {data.job}</p>
     </div>
     );
 }
