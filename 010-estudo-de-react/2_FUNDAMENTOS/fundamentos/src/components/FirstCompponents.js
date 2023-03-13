2f9b1dfa4879bc48a19b1a04732fcb2dcc9b129e
Roney Bernardes <83255781+Roney-Bernardes-Dev@users.noreply.github.com>
2023-03-13 10:32:13 -0300

    cleater TemplateExpressions.js

--- 010-estudo-de-react/2_FUNDAMENTOS/fundamentos/src/components/FirstCompponents.js
+++ 010-estudo-de-react/2_FUNDAMENTOS/fundamentos/src/components/FirstCompponents.js
@@ -2,7 +2,7 @@ const FirstComponents = () => {
     return (
         <div>
             <h1>Meu Primeiro Componente</h1>
-            
+            <p>Coisa boa funcionou</p>
         </div>
     );
 };
