88eac397ef7b316c9ab5a65a58c61c98dd01354c
Roney Bernardes <83255781+Roney-Bernardes-Dev@users.noreply.github.com>
2023-03-23 13:49:33 -0300

    update file typescript

---
+++ 005-estudo-de-typescript/typescript_angular_dio/src/index.js
@@ -0,0 +1,8 @@
+function printObjeto(pessoa) {
+    console.log(pessoa);
+}
+;
+printObjeto({
+    name: "Bruce Wayne",
+    vulgo: "Batman",
+});
---
+++ 005-estudo-de-typescript/typescript_angular_dio/src/index.ts
@@ -0,0 +1,14 @@
+type heroi = {
+    name: string;
+    vulgo: string;
+};
+
+function printObjeto(pessoa: heroi) {
+    console.log(pessoa);
+};
+
+printObjeto({
+    name: "Bruce Wayne",
+    vulgo: "Batman",
+
+})
