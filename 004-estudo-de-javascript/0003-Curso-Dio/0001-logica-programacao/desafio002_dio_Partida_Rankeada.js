1c000b20690197f4b94407e5d78737393f154329
Roney Bernardes <testeroneyb@gmail.com>
2024-10-17 13:29:26 -0300

    Update desafio002_dio_Partida_Rankeada.js

--- 004-estudo-de-javascript/0003-Curso-Dio/0001-logica-programacao/desafio002_dio_Partida_Rankeada.js
+++ 004-estudo-de-javascript/0003-Curso-Dio/0001-logica-programacao/desafio002_dio_Partida_Rankeada.js
@@ -34,3 +34,36 @@
 
 //Bons estudos 😉
 
+// Função para calcular o saldo de rankeadas e determinar o nível
+function calcularRank(vitorias, derrotas) {
+    // Calculando o saldo (vitórias - derrotas)
+    const saldoVitorias = vitorias - derrotas;
+    let nivel;
+  
+    // Estruturas de decisão para determinar o nível com base no número de vitórias
+    if (vitorias < 10) {
+      nivel = 'Ferro';
+    } else if (vitorias >= 11 && vitorias <= 20) {
+      nivel = 'Bronze';
+    } else if (vitorias >= 21 && vitorias <= 50) {
+      nivel = 'Prata';
+    } else if (vitorias >= 51 && vitorias <= 80) {
+      nivel = 'Ouro';
+    } else if (vitorias >= 81 && vitorias <= 90) {
+      nivel = 'Diamante';
+    } else if (vitorias >= 91 && vitorias <= 100) {
+      nivel = 'Lendário';
+    } else if (vitorias >= 101) {
+      nivel = 'Imortal';
+    }
+  
+    // Exibindo a mensagem final
+    console.log(`O Herói tem de saldo de ${saldoVitorias} está no nível de ${nivel}`);
+  }
+  
+  // Exemplo de uso da função
+  const vitorias = 55; // Número de vitórias
+  const derrotas = 20; // Número de derrotas
+  
+  calcularRank(vitorias, derrotas); // O Herói tem de saldo de 35 está no nível de Ouro
+  
